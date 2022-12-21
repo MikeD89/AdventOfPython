d3479fea76336af73766a0212b1ede1915455352
Mike D <michaelduller@hotmail.com>
2022-12-21 22:33:19 +0000

    day 19 start

--- 2022/2022_day_19.py
+++ 2022/2022_day_19.py
@@ -6,20 +6,19 @@ from aocd import get_data, submit
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from utils import *
 
-data = get_data(day=19, year=2022)
-testdata = get_testdata(day=19, year=2022)
-
 def strip_input(line):
-    return line.replace("  Each ore robot costs ", "") \
-               .replace("  Each clay robot costs ", "") \
-               .replace("  Each obsidian robot costs ", "") \
-               .replace("  Each geode robot costs ", "") \
-               .replace(" ore and ", ",") \
-               .replace(" ore.", "") \
-               .replace(" clay.", "") \
-               .replace(" obsidian.", "") \
-               .replace("Blueprint ", "") \
-               .replace(":", "")
+    return line.strip().replace("Each ore robot costs ", "") \
+                       .replace("Each clay robot costs ", "") \
+                       .replace("Each obsidian robot costs ", "") \
+                       .replace("Each geode robot costs ", "") \
+                       .replace(" ore and ", ",") \
+                       .replace(" ore", "") \
+                       .replace(" clay", "") \
+                       .replace(" obsidian.", "") \
+                       .replace(" obsidian!", "") \
+                       .replace("Blueprint ", "") \
+                       .replace("!", "") \
+
 
 class blueprint:
     def __init__(self, lines):
@@ -29,9 +28,31 @@ class blueprint:
         self.obsidian_cost = [int(i) for i in strip_input(lines[3]).split(",")]
         self.geode_cost = [int(i) for i in strip_input(lines[4]).split(",")]
 
+td = False
+
+data = get_data(day=19, year=2022)
+testdata = get_testdata(day=19, year=2022)
+
+input = testdata.split("\n\n") if td else  data.splitlines()
+input = [i.replace(":", "!") for i in input]
+input = [i.replace(".", "!") for i in input]
+blueprints = [blueprint(i.splitlines()) for i in input] if td else [blueprint(i.split("! ")) for i in input]
+
+def calc_blueprint_cost(bp: blueprint, minutes: int):
+    ore = 0
+    clay = 0
+
+    ore_robots = 1
+    clay_robots = 0
+
+    for min in range(minutes):
+        ore += ore_robots
+
+
 def part1():
-    input = testdata.split("\n\n")
-    blueprints = [blueprint(i.splitlines()) for i in input]
+    costs = []
+    for bp in blueprints:
+        costs.append(calc_blueprint_cost(bp, 24))
     return None
     
 def part2():
