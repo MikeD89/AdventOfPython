a04c479ffb00101c19a2a37350f47f3ad0929ba5
Mike D <michaelduller@hotmail.com>
2022-12-24 03:18:37 +0000

    day 21

--- 2022/2022_day_21.py
+++ 2022/2022_day_21.py
@@ -8,6 +8,7 @@ from utils import *
 
 data = get_data(day=21, year=2022)
 testdata = get_testdata(day=21, year=2022)
+human = "humn"
 
 monkeys = {}
 for d in data.splitlines():
@@ -31,11 +32,65 @@ def get(key):
 def part1():
     return get("root")
     
+def check():
+    value = monkeys["root"]
+    v1 = get(value[0])
+    v2 = get(value[2])
+    return v1 == v2
+
 def part2():
-    input = testdata
-    return None
+    base = monkeys.copy()
+    # work out the path from humn to root
+    path = []
+    key = "humn"
+    while key != "root":
+        for k, v in monkeys.items():
+            if type(v) == list and key in v:
+                path.append(k)
+                key = k
+                break
+
+
+    path.remove("root")
+    v1 = monkeys["root"][0]
+    v2 = monkeys["root"][2]
+    solve_for = v1
+    if v2 not in path:
+        solve_for = v2
+    target = get(solve_for)
+        
+
+    # invert the path and solve the correct answer each step
+    path.reverse()
+    for p in path:
+        v1 = monkeys[p][0]
+        op = monkeys[p][1]
+        v2 = monkeys[p][2]
+
+        solve_for = v1
+        if v2 not in path:
+            solve_for = v2
+        solve_for_v1 = solve_for == v1
+
+        other = get(solve_for)
+
+        if op == "+":
+            target = target - other
+        elif op == "-":
+            if solve_for_v1:
+                target =  other - target
+            else:
+                target = target + other
+        elif op == "*":
+            target = target / other
+        elif op == "/":
+            if solve_for_v1:
+                target =  other / target
+            else:
+                target = target * other
+    return int(target)
 
 if __name__ == "__main__":
     print("-- AoC 2022 - Day 21 --\n")
     part("One", 21, 2022, part1, False)
-    part("Two", 21, 2022, part2, False)
+    part("Two", 21, 2022, part2, True)
